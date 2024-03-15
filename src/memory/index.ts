aeeac06f2ad15b9719d67acde46eaf17d504e777
Ben Davies <bendavies@Bens-MacBook-Pro.local>
2024-03-15 16:02:46 +0000

    Memory module to read and write from memory array

--- src/memory/index.ts
+++ src/memory/index.ts
@@ -1,7 +1,30 @@
+import { readPc } from "../processor/operations/register";
+import { Register, RegisterKeys } from "../processor/types";
 import { invaders } from "./invaders"
 
-export const readWith = (memory: string[]) => (address: string) => memory[parseInt(address, 16)];
+export const getMemory = () => invaders;
+
+export const readWith = (memory: string[]) => (address: string) => {
+   
+   
+    return memory[parseInt(address, 16)] ?? "00"
+};
 export const read = readWith(invaders);
 
-export const writeWith = (memory: string[]) => (address: string, value: string) => memory[parseInt(address, 16)] = value
+export const readArg = (register: Register, argNum = 1) => {
+
+    const address = (readPc(register) + argNum).toString(16);
+
+    
+
+    const res = read(address);
+  
+    return res;
+}
+
+export const writeWith = (memory: string[]) => (address: string, value: string) => {
+    if(address.toLowerCase() == "23fd" && value.length  < 2) debugger;
+    
+    memory[parseInt(address, 16)] = value
+}
 export const write = writeWith(invaders)
